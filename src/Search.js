b2889073f761cbee8e918ddc46f21a1d40a75759
MarinaKuvshinova <pn-05@ukr.net>
2024-11-30 23:05:31 +0200

    fixed for building

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 function Search({ getInfo }) {
     function handleSubmit(e) {
         e.preventDefault();
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Search from "./Search";
 import ShowInfo from "./ShowInfo";
 import axios from "axios";
